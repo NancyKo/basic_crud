8773b1a5d12bc8af448a3279ad921d871d4762b0
lunsee <konancys@gmail.com>
2014-07-31 14:20:55 -0400

    precompile

--- config/application.rb
+++ config/application.rb
@@ -1,13 +1,19 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+require "active_model/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "sprockets/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
 module BasicCrud
   class Application < Rails::Application
+    config.autoload_paths += %W(#{config.root}/app/classes)
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
