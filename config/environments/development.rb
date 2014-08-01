a4be4cff2588e64a2bca392951a54ae493f467f1
lunsee <konancys@gmail.com>
2014-08-01 11:43:17 -0400

    changed default url host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = {host: 'localhost:3000'}
+  config.action_mailer.default_url_options = {host: 'http://http://fast-shelf-3839.herokuapp.com/'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = 
     ActionMailer::Base.smtp_settings = {
